4777b19b86e993296f849cf3a4b943b5d7b56c1e
Salma Hassaan <semsemahassaan@gmail.com>
2019-03-29 13:58:10 -0700

    Install ASP.NET Framework with Entity Framework integration

---
+++ DOWEE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DOWEE.Startup))]
+namespace DOWEE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
